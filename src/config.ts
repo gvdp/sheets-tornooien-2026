5cf36439397c9b5e2bff63d21632e2e5dbc2ecf4
gvdp <vdputteglenn@gmail.com>
2026-03-18 20:29:48 +0100

    chore: update config

--- src/config.ts
+++ src/config.ts
@@ -1,42 +1,5 @@
 // todo: this should be configurable per template clone, not included here
 
 export const SHEET_IDS: Record<string, { name: string; sheetID: string }> = {
-  U16M: {
-    sheetID: '14EV9ipFOtblZsBQ0MhNxJgw9bIPBerDibYqIPeDDkQs',
-    name: 'Meisjes U16',
-  },
-  U13M: {
-    sheetID: '1yJx9AiPBLYknWg1u1CPHvKFEYGt4m7JViSju6LjZhog',
-    name: 'Meisjes U13',
-  },
-  U11M: {
-    sheetID: '1Gf0_pyS4CGzrqXWBPQmSOoSZA3_4c_nahaL7LcBwmuQ',
-    name: 'Meisjes U11',
-  },
-  U9M: {
-    sheetID: '1-o5CXpCH9WQ37jHQQGvYAWJZjxU3sVAEyfvDH-p7ESE',
-    name: 'Meisjes U9',
-  },
-  H6V6: {
-    sheetID: '1mF_frLo2LJNzxTtPiEaYajdwoBhcSDMYxMZP3Q8VwYc',
-    name: '6 Tegen 6 Heren',
-  },
-  D6V6: {
-    sheetID: '1xZwTAAAAV_kbdpL7w76lZFAZNRvfJMFKTbAMwH8uZeA',
-    name: '6 Tegen 6 Dames',
-  },
-  TEST: {
-    name: 'TEST',
-    sheetID: '1yZhUeE23mL1dlikzuPfx6QNSI24G0BmgPU65wdiJMNI',
-  },
-  U6: { name: 'U6', sheetID: '1CxzMqPn636__xP_OxWBBFmYusM1YBz_60kJXgc_ioaQ' },
-  U7: { name: 'U7', sheetID: '1rzP6TL9hLl8wXl_HOlTf6NE_Ajvrbll4WubaeIFU3ew' },
-  U8: { name: 'U8', sheetID: '1Eeu0lO7Bk1WeBR_sUqBLIcZp1w-Z0OPJ6XYyo0IHu1o' },
-  U9: { name: 'U9', sheetID: '1jbhsdQBzDpfBopEry7ir1XxqyyDygAhycXGvb7EH3uM' },
-  U10: { name: 'U10', sheetID: '1a-AVrtEZZJnQyIgBhyBJaIbVgSFskg_1qGMffeCV_BI' },
-  U11: { name: 'U11', sheetID: '1Glc-zF0Tyh1Du4o-xF8pjzwanAa6VKz-vkIiAiOWOqs' },
-  U12: { name: 'U12', sheetID: '14tzL1EhgcF5eA6j0A73qs5AbW7x1tt5PSvoleugtax4' },
-  U13: { name: 'U13', sheetID: '1IchQtWP32k0ErBbFa3fZ4NCQmwAgMuxNcJS3KcYn6Tc' },
-  U15: { name: 'U15', sheetID: '1atr3Ywaf_0axQc0zRfocWkDOgideUpSqvpLg7vv7E8M' },
-  U17: { name: 'U17', sheetID: '1UayO6LvQVyWkGVBi99UzL97IDwtNTk0nzLoIVhBf9Io' },
+  U17: { name: 'U17', sheetID: '1pF_cTrStPQsO7zGOH7Ftn0SIlsPk_bvwVAibZuO2hXc' },
 }
