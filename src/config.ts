11a03184c257873cc06073691f5b51a75a8edcf9
gvdp <vdputteglenn@gmail.com>
2026-03-18 21:13:07 +0100

    chore: add sheet config

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,15 @@
 // todo: this should be configurable per template clone, not included here
 
 export const SHEET_IDS: Record<string, { name: string; sheetID: string }> = {
-  U17: { name: 'U17', sheetID: '1pF_cTrStPQsO7zGOH7Ftn0SIlsPk_bvwVAibZuO2hXc' },
+  U6: { name: 'U6', sheetID: '15iBj40eT3AKAD1OwhCxmvj4YaK9_qMpRdGgxmx23ims' },
+  U7: { name: 'U7', sheetID: '1WnelZxOk5yYHQzBkl_EcwHWE1qYeQO8kEDjZRFdBKjc' },
+  U8: { name: 'U8', sheetID: '1WAcrLeoFrsbfOV2T26NYgZDM5BwE1G51FShAj7x0zck' },
+  U9: { name: 'U9', sheetID: '18Y6w7v9CaOwSigKuEN1D3GwK5jA8MdiDubJOq6xQFiI' },
+  U10: { name: 'U10', sheetID: '1TTgiH-LY8iKDFYf0UAFhpMzV8MclU0MD7KghP1rPsUI' },
+  U11: { name: 'U11', sheetID: '1SnqBnzv8gYl_Vh5RT2wJDnm1nOQJqcQlFATqYiaqaLQ' },
+  U12: { name: 'U12', sheetID: '1lamK7ak5Bpo-d_B4l6m_CJBAfexYzBcc4xDBMkcy3QE' },
+  U13: { name: 'U13', sheetID: '1xanAHjg8ycIlEnCHOjdnXoc9rFJfYLbeH-Q4Slc1xAM' },
+  U15: { name: 'U15', sheetID: '1yjnjnM_U29Hiws4qaw4sF79NKoQ37XRkHspleT0yhBM' },
+  U17: { name: 'U17', sheetID: '1eT3bv2xYbgNMQ_6Pxj3uiKeH9irguPyGYJ7rcb_ZmjY' },
+  U19: { name: 'U19', sheetID: '136IPhLN4VuDZjt9RSzXMS1FHzwdP7UGLraBBcaQApVY' },
 }
